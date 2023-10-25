2fe0239520cc8f1b29a21a25b6d244bc2db78ce0
Ankit Gupta <ankit1381997@gmail.com>
2023-10-25 23:35:14 +0530

    blog remove from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -100,7 +100,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
+            {/* <Nav.Item>
               <Nav.Link
                 href="https://soumyajitblogs.vercel.app/"
                 target="_blank"
@@ -108,11 +108,11 @@ function NavBar() {
               >
                 <ImBlog style={{ marginBottom: "2px" }} /> Blogs
               </Nav.Link>
-            </Nav.Item>
+        </Nav.Item> */}
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/ankit13819"
                 target="_blank"
                 className="fork-btn-inner"
               >
