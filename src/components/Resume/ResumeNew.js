7514e252554c08b31030f59ac464ef6eb019abf2
Ankit Gupta <ankit1381997@gmail.com>
2024-02-15 14:29:24 +0530

    resume update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Ankit_Gupta_CV.pdf";
+import pdf from "../../Assets/../Assets/Ankit_Gupta_New.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
