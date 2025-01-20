7c6063bc4e6d99fbd3f59ed99a5cdc2026669ffb
ankit13819 <ankit1381997@gmail.com>
2025-01-20 08:45:17 +0530

    about page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,5 @@
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
 import { Montserrat } from 'next/font/google'
 import Head from 'next/head'
@@ -15,11 +17,10 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${montserrat.variable} font-sans bg-light w-full min-h-screen`}>
+        <Navbar />
         <Component {...pageProps} />
+        <Footer />
       </main>
     </>
-
-
-https://www.youtube.com/watch?v=Yw7yWHigGKI
   )
 }
